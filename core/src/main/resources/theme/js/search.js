1a314d1420648d61ebd4063b426cee08f3bb3a0b
evol <gevol1212@gmail.com>
2026-06-24 00:56:01 +0800

    Remove legacy theme resources

--- core/src/main/resources/theme/js/search.js
+++
@@ -1,5 +0,0 @@
-(function () {
-    if (window.JavadocSearch && typeof window.JavadocSearch.updateSearchResults === "function") {
-        window.updateSearchResults = window.JavadocSearch.updateSearchResults;
-    }
-})();
